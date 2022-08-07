428c9237660e2bd8dc01b248127086cbc309235a
sarahslopes <sarah_lopes@discente.ufg.br>
2022-08-07 11:59:40 -0300

    Tabela apartamento criada

--- src/main/java/com/br/ecxus/ReservJa/model/Apartament.java
+++ src/main/java/com/br/ecxus/ReservJa/model/Apartament.java
@@ -1,11 +1,13 @@
 package com.br.ecxus.ReservJa.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,6 +18,8 @@ import lombok.NoArgsConstructor;
  * Apartament
  */
 
+@Entity
+@Table(name = "APARTAMENT")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
